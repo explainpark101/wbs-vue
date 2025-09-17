3a025b9ff5e0f27b1de54a836cf614bddf71e1f2
박재형 <47944126+explainpark101@users.noreply.github.com>
2025-09-17 02:01:57 +0000

    default setting

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Wbs from './components/Wbs.vue'
+</script>
+
+<template>
+  <main class="container">
+    <Wbs />
+  </main>
+</template>
+
---
+++ src/components/Wbs.vue
@@ -0,0 +1,5 @@
+<template>
+    <article>
+        <h1>Hello, World!</h1>
+    </article>
+</template>
